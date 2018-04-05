38d6ed2366c971af4bca6c17b9b00539c3145e58
rom1212 <21117906+rom1212@users.noreply.github.com>
2018-04-05 09:58:10 -0700

    Update pprint-json.py

--- python/pprint-json.py
+++ python/pprint-json.py
@@ -11,3 +11,4 @@ with open (filename, "r") as myfile:
     data=myfile.read()
     parsed = json.loads(data)
     print json.dumps(parsed, indent=4, sort_keys=True)
+    # json.dump(parsed, file_descripter, indent=4, sort_keys=True)  # this can write directly to file.
